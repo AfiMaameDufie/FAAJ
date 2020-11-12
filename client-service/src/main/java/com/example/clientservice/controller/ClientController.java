5c3340abc3482ede1ff9537f9a63012516cb220a
Afi Maame Dufie <afimaamedufie@gmail.com>
2020-11-12 18:40:13 +0000

    Added Portfolio and Stock microservices

--- client-service/src/main/java/com/example/clientservice/controller/ClientController.java
+++ client-service/src/main/java/com/example/clientservice/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.example.clientservice.controller;
 
 import com.example.clientservice.model.Client;
+import com.example.clientservice.model.Portfolio;
+import com.example.clientservice.model.Stock;
 import com.example.clientservice.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -53,4 +56,14 @@ public class ClientController {
     public void removeClientById(@PathVariable int clientId) {
         clientService.removeClient(clientId);
     }
+
+    @PostMapping(path = "/client/{id}/portfolios")
+    public List<Portfolio> createPortfolio(@RequestBody List<Stock> stocks, @PathVariable int id){
+//        Stock stock = new Stock(1, "IBM");
+//        Stock stk = new Stock(2, "APPL");
+//        List<Stock> stocks = new ArrayList<>();
+        return clientService.createPortfolio(id, stocks);
+    }
+
+
 }
